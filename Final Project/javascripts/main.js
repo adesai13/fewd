b4a172a8aac33cb62364d73da1369a0113b7296e
Arjun Desai <adesai13@gmail.com>
2015-05-28 14:57:17 -0700

    Added FB button to new window

--- Final Project/javascripts/main.js
+++ Final Project/javascripts/main.js
@@ -24,6 +24,16 @@ $('a.tweet').click(function(e) {
         });
       });
 
+  // Facebook Share Button
+
+  function fbShare(url, title, descr, image, winWidth, winHeight) {
+
+    var winTop = (screen.height / 2) - (winHeight / 2);
+    var winLeft = (screen.width / 2) - (winWidth / 2);
+    var descr = $('p.quote').text()   
+    window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+}
+
 // Go back function--------------------------------------//
 $(".goback").click(function() {
   $(".quotebox").fadeOut(600, function() {
