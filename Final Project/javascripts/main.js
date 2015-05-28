0b7d0559c911f039289027e9ea7b7d6ae565165d
Arjun Desai <adesai13@gmail.com>
2015-05-28 16:22:29 -0700

    ALMOST FINAL!

--- Final Project/javascripts/main.js
+++ Final Project/javascripts/main.js
@@ -16,23 +16,22 @@ $('a.tweet').click(function(e) {
         window.open('http://twitter.com/share?text=' + title + ' – '+ author + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
       });
 
-
-      // Click function----------------------------------------//
-      $("td").click(function() {
-        $(".table").fadeOut(600, function() {
-          $(".quotebox").fadeIn(600);
-        });
-      });
-
   // Facebook Share Button
 
   function fbShare(url, title, descr, image, winWidth, winHeight) {
 
     var winTop = (screen.height / 2) - (winHeight / 2);
     var winLeft = (screen.width / 2) - (winWidth / 2);
-    
+
     window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
-}
+      }
+
+// Click function----------------------------------------//
+$("td").click(function() {
+  $(".table").fadeOut(600, function() {
+    $(".quotebox").fadeIn(600);
+  });
+});
 
 // Go back function--------------------------------------//
 $(".goback").click(function() {
